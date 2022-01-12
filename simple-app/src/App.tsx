9467db7287a5ce30b23d05f598c0555dc20f6a64
Nathan Gilbert <nathan.gilbert@gmail.com>
2022-01-11 18:11:59 -0700

    updates for input box and search button

--- simple-app/src/App.tsx
+++ simple-app/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import styled from "styled-components"
+import logo from "./logo.svg"
+import "./App.css"
+
+// Styling a regular HTML input
+const StyledInput = styled.input`
+  display: block;
+  margin: 20px 0px;
+  border: 2px solid gray;
+  padding: 5px;
+`
+
+// Creating a custom hook
+function useInput(defaultValue: string) {
+  const [value, setValue] = useState(defaultValue)
+  function onChange(e: any) {
+    setValue(e.target.value)
+  }
+  return {
+    value,
+    onChange,
+  }
+}
 
 function App() {
+  const inputProps = useInput("")
+
+  const submitHandler = (e: any) => {
+    e.preventDefault() // It will hold the form submit
+    console.log("state:", inputProps.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <form>
+            <StyledInput {...inputProps} placeholder="Type in here" />
+            <span>Value: {inputProps.value} </span>
+            <button onClick={submitHandler}>Search</button>
+          </form>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
